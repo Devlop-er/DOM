f23beaf9243c4f7f76f7d35e8933f4332f44a259
Devlop-Er <46845666+Devlop-er@users.noreply.github.com>
2019-04-02 16:25:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 // Variables pour stocker les informations
-
 var button = document.querySelector("button");
 var texte = document.getElementById("tache");
 var ul = document.querySelector("ul");
 
+// Fonction qui permet de retourner la longueur de la variable texte
 function textLength() {
     return texte.value.length;
 }
 
+// Fonction qui permet d'ajouter du contenu 
 function textInput() {
     var li = document.createElement("li");
-    // Insertion du texte taper par l'utilisateur
     li.appendChild(document.createTextNode(texte.value));
     ul.appendChild(li);
     texte.value = "";
 }
 
-// Ajouter un évènement au click sur le "button"
-
-button.addEventListener("click", function() {
-    // Condition si la valeur est supérieur à 0 executer le code sinon afficher une alert
+// Fonction qui permet d'ajouter du contenu après le Click
+function ajouterListClick() {
     if (textLength() > 0) {
         textInput();
-    } else {
-    }       
-})
+    } 
+}
 
-texte.addEventListener("keypress", function(event) {
-    // Condition si la valeur est supérieur à 0 executer le code sinon afficher une alert
+// Fonction qui permet d'ajouter du contenu après la touche Enter
+function ajouterListKeyboard(event) {
     if (textLength() > 0 && event.keyCode === 13) {
         textInput();
-    } else {
-    }       
-})
+    }
+}
 
+button.addEventListener("click", ajouterListClick);
+texte.addEventListener("keypress", ajouterListKeyboard);
