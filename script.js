5fce6a46e786d9a760979764c3413ad0acdf2f03
Devlop-Er <46845666+Devlop-er@users.noreply.github.com>
2019-04-02 15:24:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,35 @@ var button = document.querySelector("button");
 var texte = document.getElementById("tache");
 var ul = document.querySelector("ul");
 
+function textLength() {
+    return input.value.length;
+}
+
+function textInput() {
+    var li = document.createElement("li");
+    // Insertion du texte taper par l'utilisateur
+    li.appendChild(document.createTextNode(texte.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
 // Ajouter un évènement au click sur le "button"
 
 button.addEventListener("click", function() {
 
     // Condition si la valeur est supérieur à 0 executer le code sinon afficher une alert
-    if (texte.value.length > 0) {
-        var li = document.createElement("li");
-        // Insertion du texte taper par l'utilisateur
-        li.appendChild(document.createTextNode(texte.value));
-        ul.appendChild(li);
-        input.value = "";
+    if (textLength() > 0) {
+        textInput();
+    } else {
+        alert("La case est vide...")
+    }       
+})
+
+text.addEventListener("keypress", function(event) {
+
+    // Condition si la valeur est supérieur à 0 executer le code sinon afficher une alert
+    if (textLength() > 0 && event.keycode === 13) {
+        textInput();
     } else {
         alert("La case est vide...")
     }       
