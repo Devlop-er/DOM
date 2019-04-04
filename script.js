6192f9e34013e575f8a012f1f2c6d7e25830d0f4
Devlop-Er <46845666+Devlop-er@users.noreply.github.com>
2019-04-04 12:37:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var button = document.querySelector("button");
 var texte = document.getElementById("tache");
 var ul = document.querySelector("ul");
+var li = document.querySelector("li");
 
 // Fonction qui permet de retourner la longueur de la variable texte
 function textLength() {
@@ -14,7 +15,21 @@ function textInput() {
     li.appendChild(document.createTextNode(texte.value));
     ul.appendChild(li);
     texte.value = "";
+
+    var button = document.createElement("button")
+    button.appendChild(document.createTextNode("Ok"));
+    li.appendChild(button);
+    button.onclick = underlineParent;
 }
+
+function underlineParent(event){
+	event.target.parentNode.classList.toggle("done");
+}
+
+
+function removeParent(evt){
+	evt.target.parentNode.remove();
+} 
 
 // Fonction qui permet d'ajouter du contenu après le Click
 function ajouterListClick() {
